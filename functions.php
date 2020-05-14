b52f76e348ad91c3b98db4ce9e627f1da63e2c99
unknown <khanvuthy88@gmail.com>
2020-05-14 20:45:03 +0700

    Add og tag to single post

--- functions.php
+++ functions.php
@@ -229,3 +229,35 @@ function my_get_posts( $query ) {
 		$query->set( 'post_type', array( 'post', 'video') );
 	return $query;
 }
+
+//Adding the Open Graph in the Language Attributes
+function add_opengraph_doctype( $output ) {
+    return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+}
+add_filter('language_attributes', 'add_opengraph_doctype');
+ 
+//Lets add Open Graph Meta Info
+ 
+function insert_fb_in_head() {
+    global $post;
+    if (is_single() && 'post' == get_post_type()){
+    	$des_post = strip_tags( $post->post_content );
+        $des_post = strip_shortcodes( $des_post );
+        $des_post = str_replace( array("\n", "\r", "\t"), ' ', $des_post );
+        $des_post = mb_substr( $des_post, 0, 300, 'utf8' );
+        echo '<meta property="og:title" content="' . get_the_title() . '"/>';
+        echo '<meta property="og:type" content="article"/>';
+        echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+        echo '<meta property="og:description" content="'. $des_post.'" />';
+        echo '<meta property="og:site_name" content="'. get_bloginfo('name') .'"/>';
+	    if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+	        $default_image= get_template_directory_uri().'/assets/images/logo.png'; //replace this with a default image on your server or an image in your media library
+	        echo '<meta property="og:image" content="' . $default_image . '"/>';
+	    }
+	    else{
+	        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+	        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+	    }
+    }
+}
+add_action( 'wp_head', 'insert_fb_in_head', 5 );
